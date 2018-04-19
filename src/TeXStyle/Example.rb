5462ff15fad66eb39f5daeea530b70011f95ba23
Gautam Chaudhuri <14055370+QuantumAnon@users.noreply.github.com>
2018-04-20 00:20:41 +0100

    added listings environment example

---
+++ src/TeXStyle/Example.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+def sayHello(a)
+    puts("Hello " + a)
+end
+
+sayHello("Gautam") #> "Hello Gautam"
+
+# Very long lines look like this "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
