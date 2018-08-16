b729429b4e97caba1ccdb40e929e49a780c6d712
Gautam Chaudhuri <14055370+QuantumAnon@users.noreply.github.com>
2018-08-16 20:17:02 +0100

    build script now has a simple name deciding feature

--- buildAll.sh
+++ buildAll.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
+
+JOBNAME="test"
+
+if [[ -n "$1" ]]; then
+  JOBNAME="$1"
+fi
+
+echo "Compiling with jobname $JOBNAME"
 
 cd ./src/TeXStyle
-xelatex -output-directory=out -jobname=test build.dtx
+xelatex -output-directory=out -jobname="$JOBNAME" build.dtx
 cd ../TeXStyle-article
-xelatex -output-directory=out -jobname=test build.dtx
+xelatex -output-directory=out -jobname="$JOBNAME""-article" build.dtx
